6e741010fa3b008354544bd72e7a49f753271aba
GodnTan <137321712+GodnTan@users.noreply.github.com>
2024-03-03 16:58:54 +0800

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -12,10 +12,6 @@ def calculate_demographic_data(print_data=True):
     average_age_men = round(df.loc[df['sex'] == 'Male', 'age'].mean(), 1)
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = round(df['education'].value_counts(normalize=True)['Bachelors']*100.0,1)
-
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    
-    # What percentage of people without advanced education make more than 50K?
   
     # with and without `Bachelors`, `Masters`, or `Doctorate` 
     higher_education = df.loc[df['education'].isin(['Bachelors','Masters','Doctorate'])]
